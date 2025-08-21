754d8aec7a872dac5ccf06bbb3e7fcefb72007f2
boommanpro <boommanpro@gmail.com>
2025-08-21 20:40:27 +0800

    feat(支持url传参使用):

--- src/containers/editor/components/InitialSetup.tsx
+++ src/containers/editor/components/InitialSetup.tsx
@@ -1,8 +1,10 @@
+import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { setupGlobalGraphStyle } from "@/lib/graph/layout";
 import { px2num } from "@/lib/utils";
 import { type MyWorker } from "@/lib/worker/worker";
 import { useConfigFromCookies } from "@/stores/hook";
+import { useEditorStore } from "@/stores/editorStore";
 import { useStatusStore } from "@/stores/statusStore";
 import { useUserStore } from "@/stores/userStore";
 import { wrap } from "comlink";
@@ -31,6 +33,8 @@ function useInitial() {
       updateActiveOrder: state.updateActiveOrder,
     })),
   );
+  const searchParams = useSearchParams();
+  const mainEditor = useEditorStore((state) => state.main);
 
   useEffect(() => {
     updateActiveOrder(user);
@@ -66,4 +70,18 @@ function useInitial() {
     window.worker.setupGlobalGraphStyle(measured);
     console.l("finished measuring graph base style:", measured);
   }, []);
+
+  // 从URL参数读取data并填充到编辑器
+  useEffect(() => {
+    const data = searchParams.get("data");
+    if (data && mainEditor) {
+      mainEditor.parseAndSet(data);
+      // 清除URL参数
+      if (typeof window !== "undefined") {
+        const url = new URL(window.location.href);
+        url.searchParams.delete("data");
+        window.history.replaceState({}, "", url.toString());
+      }
+    }
+  }, [searchParams, mainEditor]);
 }
